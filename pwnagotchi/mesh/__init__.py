fae6115e445c65f0d61db46e8526ff7dbb1e8813
Simone Margaritelli <evilsocket@gmail.com>
2019-10-06 16:36:25 +0200

    fix: using sha256 for the public key fingerprint

--- pwnagotchi/mesh/__init__.py
+++ pwnagotchi/mesh/__init__.py
@@ -11,4 +11,4 @@ def get_identity(config):
     pubkey = None
     with open(config['main']['pubkey']) as fp:
         pubkey = RSA.importKey(fp.read())
-    return pubkey, hashlib.sha1(pubkey.exportKey('DER')).hexdigest()
+    return pubkey, hashlib.sha256(pubkey.exportKey('DER')).hexdigest()
