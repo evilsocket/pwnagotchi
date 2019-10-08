0c81d25aec21bfe52d6a4fd996a2c11c4e7305bb
Simone Margaritelli <evilsocket@gmail.com>
2019-10-08 17:04:46 +0200

    misc: small fix or general refactoring i did not bother commenting

--- pwnagotchi/plugins/default/screen_refresh.py
+++ pwnagotchi/plugins/default/screen_refresh.py
@@ -6,22 +6,19 @@ __description__ = 'Refresh he e-ink display after X amount of updates'
 
 import logging
 
-from pwnagotchi.ui.components import LabeledValue
-from pwnagotchi.ui.view import BLACK
-import pwnagotchi.ui.fonts as fonts
-
 OPTIONS = dict()
 update_count = 0;
+
 
 def on_loaded():
     logging.info("Screen refresh plugin loaded")
 
+
 def on_ui_update(ui):
-    global update_count    
+    global update_count
     update_count += 1
     if update_count == OPTIONS['refresh_interval']:
         ui._init_display()
         ui.set('status', "Screen cleaned")
         logging.info("Screen refreshing")
         update_count = 0
-
