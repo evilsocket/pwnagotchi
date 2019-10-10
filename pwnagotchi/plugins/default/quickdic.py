e48f9bfcc766a7e573e6aa1a565466ea29152232
root <root@RossDebian>
2019-10-10 23:34:15 +0100

    code tidy

--- pwnagotchi/plugins/default/quickdic.py
+++ pwnagotchi/plugins/default/quickdic.py
@@ -6,8 +6,9 @@ __description__ = 'Run a quick dictionary scan against captured handshakes'
 
 '''
 Aircrack-ng needed, to install:
->apt-get install aircrak-ng
-Upload worrdlists files in .txt forrmat to folder in config file (default: /opt/wordlists/)
+> apt-get install aircrack-ng
+Upload wordlist files in .txt format to folder in config file (Default: /opt/wordlists/)
+Cracked handshakes stored in handshake folder as [essid].pcap.cracked 
 '''
 
 import logging
@@ -29,7 +30,7 @@ def on_handshake(agent, filename, access_point, client_station):
         logging.info("[quickdic] No handshake")
     else:
         logging.info("[quickdic] Handshake confirmed")
-        result2 = subprocess.run(('aircrack-ng -w '+OPTIONS['wordlist_folder']+'*.txt -q -b '+result+' '+filename+' | grep KEY'),shell=True,stdout=subprocess.PIPE)
+        result2 = subprocess.run(('aircrack-ng -w '+OPTIONS['wordlist_folder']+'*.txt -l '+filename+'.cracked -q -b '+result+' '+filename+' | grep KEY'),shell=True,stdout=subprocess.PIPE)
         result2 = result2.stdout.decode('utf-8').strip()
         logging.info("[quickdic] "+result2)
         if result2 != "KEY NOT FOUND":
@@ -43,11 +44,9 @@ text_to_set = "";
 def set_text(text):
     global text_to_set
     text_to_set = text
-    logging.info('[quickdic] setText: '+text)
 
 def on_ui_update(ui):
     global text_to_set
     if text_to_set:
-        logging.info('[quickdic] ui_update: '+text_to_set)
         ui.set('status', text_to_set)
         text_to_set = ""
