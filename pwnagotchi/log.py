ba7c0ee4e6443594494c9d2783abf5bcbfdbec19
Simone Margaritelli <evilsocket@gmail.com>
2019-10-13 18:29:56 +0200

    misc: small fix or general refactoring i did not bother commenting

--- pwnagotchi/log.py
+++ pwnagotchi/log.py
@@ -129,10 +129,12 @@ class LastSession(object):
                 if m:
                     name, pubkey, rssi, sid, pwnd_tot, uptime = m[0]
                     if pubkey not in cache:
-                        self.last_peer = Peer(sid, 1, int(rssi),
-                                              {'name': name,
-                                               'identity': pubkey,
-                                               'pwnd_tot': int(pwnd_tot)})
+                        self.last_peer = Peer({
+                            'session_id': sid,
+                            'channel': 1,
+                            'rssi': int(rssi),
+                            'identity': pubkey,
+                            'advertisement':{'pwnd_tot': int(pwnd_tot)}})
                         self.peers += 1
                         cache[pubkey] = self.last_peer
                     else:
