9aca3a3a5b0af6d8fa0788065f0991ca0bcfa197
Casey Diemel <diemelcw@gmail.com>
2019-10-13 17:53:18 -0400

    added example for testing
    Signed-off-by: Casey Diemel <diemelcw@gmail.com>

---
+++ pwnagotchi/plugins/default/unfiltered_example.py
@@ -0,0 +1,18 @@
+__author__ = 'diemelcw@gmail.com'
+__version__ = '1.0.0'
+__name__ = 'unfiltered_example'
+__license__ = 'GPL3'
+__description__ = 'An example plugin for pwnagotchi that implements on_unfiltered_ap_list(agent,aps)'
+
+import logging
+
+# Will be set with the options in config.yml config['main']['plugins'][__name__]
+OPTIONS = dict()
+
+# called when AP list is ready, before whitelist filtering has occured
+def on_unfiltered_ap_list(agent,aps):
+    logging.info("Unfiltered AP list to follow")
+    for ap in aps:
+        logging.info(ap['hostname'])
+    
+    ## Additional logic here ##
