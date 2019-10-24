6e3e12a2b22c76d7e5a4f95257cc6b1a98694eaf
n0m4d1k <48110748+n0m4d1k@users.noreply.github.com>
2019-10-24 15:30:27 -0400

    Add user input for interfaces
    clarifies interfaces section and allows for user input.

--- scripts/linux_connection_share.sh
+++ scripts/linux_connection_share.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+#prompt user to input pwnagotchi ethernet interface on host and input host internet interface
+read -p 'Input pwnagotchi ethernet interface ex:"enp0s20f0u1": ' pwninterface
+read -p 'Input host internet connected interface ex:"wlan0": ' hostinterface
+
 # name of the ethernet gadget interface on the host
-USB_IFACE=${1:-enp0s20f0u1}
+USB_IFACE=${1:-$pwninterface}
 USB_IFACE_IP=10.0.0.1
 USB_IFACE_NET=10.0.0.0/24
 # host interface to use for upstream connection
-UPSTREAM_IFACE=${2:-enxe4b97aa99867}
+UPSTREAM_IFACE=${2:-$hostinterface}
 
 ip addr add "$USB_IFACE_IP/24" dev "$USB_IFACE"
 ip link set "$USB_IFACE" up
