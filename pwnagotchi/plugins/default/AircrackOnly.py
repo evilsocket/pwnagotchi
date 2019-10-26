26f88396dbc849d6c788bffb0546a3db3eca53da
Robin B <robin@broda.me>
2019-10-27 01:39:34 +0200

    plugins/AircrackOnly: Only parse pcap once and don't delete pcaps with more than one handshake
    This refactors AircrackOnly to:
    - parse the pcap only once to increase efficiency
    - not delete pcaps containing more than one handshake (just in case)
    - log uncrackable pcaps to `logging.info`
    
    Signed-off-by: Robin Broda <robin@broda.me>

--- pwnagotchi/plugins/default/AircrackOnly.py
+++ pwnagotchi/plugins/default/AircrackOnly.py
@@ -13,32 +13,23 @@ import logging
 import subprocess
 import string
 import os
+import ast
 
 OPTIONS = dict()
 
 def on_loaded():
-    logging.info("aircrackonly plugin loaded")
+    logging.info("[AircrackOnly] plugin loaded")
 
 def on_handshake(agent, filename, access_point, client_station):
     display = agent._view
-    todelete = 0
 
-    result = subprocess.run(('/usr/bin/aircrack-ng '+ filename +' | grep "1 handshake" | awk \'{print $2}\''),shell=True, stdout=subprocess.PIPE)
-    result = result.stdout.decode('utf-8').translate({ord(c) :None for c in string.whitespace})
-    if result:
-        logging.info("[AircrackOnly] contains handshake")
+    result = ast.literal_eval(subprocess.run(('/usr/bin/aircrack-ng '+filename+' | gawk \'/[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}/ {printf "["} match($0, /([0-9]+) handshake/, hs) {printf hs[1] ","} /handshake)/ {printf "False"} /with PMKID/ {printf "True"} /[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}/ {print "]"}\''), shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8'))
+    if result[0] > 0:
+        logging.info("[AircrackOnly] contains "+str(result[0])+" handshake(s)")
+    elif result[1]:
+        logging.info("[AircrackOnly] contains PMKID")
     else:
-        todetele = 1
-
-    if todelete == 0:
-        result = subprocess.run(('/usr/bin/aircrack-ng '+ filename +' | grep "PMKID" | awk \'{print $2}\''),shell=True, stdout=subprocess.PIPE)
-        result = result.stdout.decode('utf-8').translate({ord(c) :None for c in string.whitespace})
-        if result:
-            logging.info("[AircrackOnly] contains PMKID")
-        else:
-            todetele = 1
-
-    if todelete == 1:
+        logging.info("[AircrackOnly] contains nothing useful")
         os.remove(filename)
         set_text("uncrackable pcap")
         display.update(force=True)
