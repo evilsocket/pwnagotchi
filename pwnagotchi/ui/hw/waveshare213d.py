7fd31e14f900353b9c49ea6fef434e9698a54bec
Nikhil Jha <hi@nikhiljha.com>
2019-10-27 14:32:23 -0700

    fix resolution
    Signed-off-by: Nikhil Jha <hi@nikhiljha.com>

--- pwnagotchi/ui/hw/waveshare213d.py
+++ pwnagotchi/ui/hw/waveshare213d.py
@@ -10,46 +10,25 @@ class Waveshare213d(DisplayImpl):
         self._display = None
 
     def layout(self):
-        if self.config['color'] == 'black':
-            fonts.setup(10, 9, 10, 35)
-            self._layout['width'] = 250
-            self._layout['height'] = 122
-            self._layout['face'] = (0, 40)
-            self._layout['name'] = (5, 20)
-            self._layout['channel'] = (0, 0)
-            self._layout['aps'] = (28, 0)
-            self._layout['uptime'] = (185, 0)
-            self._layout['line1'] = [0, 14, 250, 14]
-            self._layout['line2'] = [0, 108, 250, 108]
-            self._layout['friend_face'] = (0, 92)
-            self._layout['friend_name'] = (40, 94)
-            self._layout['shakes'] = (0, 109)
-            self._layout['mode'] = (225, 109)
-            self._layout['status'] = {
-                'pos': (125, 20),
-                'font': fonts.Medium,
-                'max': 20
-            }
-        else:
-            fonts.setup(10, 8, 10, 25)
-            self._layout['width'] = 212
-            self._layout['height'] = 104
-            self._layout['face'] = (0, 26)
-            self._layout['name'] = (5, 15)
-            self._layout['channel'] = (0, 0)
-            self._layout['aps'] = (28, 0)
-            self._layout['uptime'] = (147, 0)
-            self._layout['line1'] = [0, 12, 212, 12]
-            self._layout['line2'] = [0, 92, 212, 92]
-            self._layout['friend_face'] = (0, 76)
-            self._layout['friend_name'] = (40, 78)
-            self._layout['shakes'] = (0, 93)
-            self._layout['mode'] = (187, 93)
-            self._layout['status'] = {
-                'pos': (91, 15),
-                'font': fonts.Medium,
-                'max': 20
-            }
+        fonts.setup(10, 8, 10, 25)
+        self._layout['width'] = 212
+        self._layout['height'] = 104
+        self._layout['face'] = (0, 26)
+        self._layout['name'] = (5, 15)
+        self._layout['channel'] = (0, 0)
+        self._layout['aps'] = (28, 0)
+        self._layout['uptime'] = (147, 0)
+        self._layout['line1'] = [0, 12, 212, 12]
+        self._layout['line2'] = [0, 92, 212, 92]
+        self._layout['friend_face'] = (0, 76)
+        self._layout['friend_name'] = (40, 78)
+        self._layout['shakes'] = (0, 93)
+        self._layout['mode'] = (187, 93)
+        self._layout['status'] = {
+            'pos': (91, 15),
+            'font': fonts.Medium,
+            'max': 20
+        }
         return self._layout
 
     def initialize(self):
@@ -64,5 +43,5 @@ class Waveshare213d(DisplayImpl):
         self._display.display(buf)
 
     def clear(self):
-        pass
-        #self._display.Clear()
+        #pass
+        self._display.Clear()
