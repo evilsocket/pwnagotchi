8dd5007d6b95bbd5808dcb4293d99dc4df586fd6
Dispsylala <marcus.watson@loumiaconsulting.com>
2019-11-13 01:41:06 +0000

    Remerging inbox/profile code

--- pwnagotchi/ui/web/handler.py
+++ pwnagotchi/ui/web/handler.py
@@ -1,25 +1,31 @@
 import logging
 import os
+import base64
 import _thread
+import secrets
+from functools import wraps
 
 # https://stackoverflow.com/questions/14888799/disable-console-messages-in-flask-server
 logging.getLogger('werkzeug').setLevel(logging.ERROR)
 os.environ['WERKZEUG_RUN_MAIN'] = 'true'
 
 import pwnagotchi
-import pwnagotchi.ui.web as web
 import pwnagotchi.grid as grid
+import pwnagotchi.ui.web as web
 from pwnagotchi import plugins
 
 from flask import send_file
+from flask import Response
 from flask import request
 from flask import jsonify
 from flask import abort
+from flask import redirect
 from flask import render_template, render_template_string
 
 
 class Handler:
-    def __init__(self, agent, app):
+    def __init__(self, config, agent, app):
+        self._config = config
         self._agent = agent
         self._app = app
         self._app.add_url_rule('/', 'index', self.index)
@@ -28,12 +34,34 @@ class Handler:
         self._app.add_url_rule('/shutdown', 'shutdown', self.shutdown, methods=['POST'])
         self._app.add_url_rule('/restart', 'restart', self.restart, methods=['POST'])
 
+        # inbox
+        self._app.add_url_rule('/inbox', 'inbox', self.with_auth(self.inbox))
+        self._app.add_url_rule('/inbox/profile', 'inbox_profile', self.with_auth(self.inbox_profile))
+        self._app.add_url_rule('/inbox/<id>', 'show_message', self.with_auth(self.show_message))
+        self._app.add_url_rule('/inbox/<id>/<mark>', 'mark_message', self.with_auth(self.mark_message))
+        self._app.add_url_rule('/inbox/new', 'new_message', self.with_auth(self.new_message))
+        self._app.add_url_rule('/inbox/send', 'send_message', self.with_auth(self.send_message), methods=['POST'])
+
         # plugins
-        self._app.add_url_rule('/plugins', 'plugins', self.plugins, strict_slashes=False,
+        plugins_with_auth = self.with_auth(self.plugins)
+        self._app.add_url_rule('/plugins', 'plugins', plugins_with_auth, strict_slashes=False,
                                defaults={'name': None, 'subpath': None})
-        self._app.add_url_rule('/plugins/<name>', 'plugins', self.plugins, strict_slashes=False,
+        self._app.add_url_rule('/plugins/<name>', 'plugins', plugins_with_auth, strict_slashes=False,
                                methods=['GET', 'POST'], defaults={'subpath': None})
-        self._app.add_url_rule('/plugins/<name>/<path:subpath>', 'plugins', self.plugins, methods=['GET', 'POST'])
+        self._app.add_url_rule('/plugins/<name>/<path:subpath>', 'plugins', plugins_with_auth, methods=['GET', 'POST'])
+
+    def _check_creds(self, u, p):
+        # trying to be timing attack safe
+        return secrets.compare_digest(u, self._config['username']) and \
+                secrets.compare_digest(p, self._config['password'])
+
+    def with_auth(self, f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            auth = request.authorization
+            if not auth or not auth.username or not auth.password or not self._check_creds(auth.username, auth.password):
+                return Response('Unauthorized', 401, {'WWW-Authenticate': 'Basic realm="Unauthorized"'})
+            return wrapper
 
     def _return_json(self):
 
@@ -86,21 +114,109 @@ class Handler:
         return jsonify(result)
 
     def _return_png(self):
-        # TODO - can we avoid writing to a file then reading, or keep it in memory?
         with web.frame_lock:
             return send_file(web.frame_path, mimetype="image/png")
 
     def index(self):
-
         theme = "theme-default.html"
-
         theme_config_location = self._agent.config()["ui"]["display"]["video"]
 
         if "theme" in theme_config_location:
             theme = "theme-" + theme_config_location["theme"] + ".html"
 
         return render_template(theme, title=pwnagotchi.name(),
-                               other_mode='AUTO' if self._agent.mode == 'manual' else 'MANU')
+                               other_mode='AUTO' if self._agent.mode == 'manual' else 'MANU',
+                               fingerprint=self._agent.fingerprint())
+
+    def inbox(self):
+        page = request.args.get("p", default=1, type=int)
+        inbox = {
+            "pages": 1,
+            "records": 0,
+            "messages": []
+        }
+        error = None
+
+        try:
+            inbox = grid.inbox(page, with_pager=True)
+        except Exception as e:
+            logging.exception('error while reading pwnmail inbox')
+            error = str(e)
+
+        return render_template('inbox.html',
+                               name=pwnagotchi.name(),
+                               page=page,
+                               error=error,
+                               inbox=inbox)
+
+    def inbox_profile(self):
+        data = {}
+        error = None
+
+        try:
+            data = grid.get_advertisement_data()
+        except Exception as e:
+            logging.exception('error while reading pwngrid data')
+            error = str(e)
+
+        return render_template('profile.html',
+                               name=pwnagotchi.name(),
+                               fingerprint=self._agent.fingerprint(),
+                               data=data,
+                               error=error)
+
+    def inbox_peers(self):
+        peers = {}
+        error = None
+
+        try:
+            peers = grid.memory()
+        except Exception as e:
+            logging.exception('error while reading pwngrid peers')
+            error = str(e)
+
+        return render_template('peers.html',
+                               name=pwnagotchi.name(),
+                               peers=peers,
+                               error=error)
+
+    def show_message(self, id):
+        message = {}
+        error = None
+
+        try:
+            message = grid.inbox_message(id)
+            if message['data']:
+                message['data'] = base64.b64decode(message['data']).decode("utf-8")
+        except Exception as e:
+            logging.exception('error while reading pwnmail message %d' % int(id))
+            error = str(e)
+
+        return render_template('message.html',
+                               name=pwnagotchi.name(),
+                               error=error,
+                               message=message)
+
+    def new_message(self):
+        to = request.args.get("to", default="")
+        return render_template('new_message.html', to=to)
+
+    def send_message(self):
+        to = request.form["to"]
+        message = request.form["message"]
+        error = None
+
+        try:
+            grid.send_message(to, message)
+        except Exception as e:
+            error = str(e)
+
+        return jsonify({"error": error})
+
+    def mark_message(self, id, mark):
+        logging.info("marking message %d as %s" % (int(id), mark))
+        grid.mark_message(id, mark)
+        return redirect("/inbox")
 
     def state(self, format):
         if format not in ["json", "png"]:
