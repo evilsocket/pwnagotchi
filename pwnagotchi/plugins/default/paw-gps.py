56cc872daa4b8e69905e0ae1af499e8077f4e70e
Arttumiro <miro.tammelin@hotmail.fi>
2019-11-14 22:29:58 +0200

    Update paw-gps.py

--- pwnagotchi/plugins/default/paw-gps.py
+++ pwnagotchi/plugins/default/paw-gps.py
@@ -24,7 +24,7 @@ class PawGPS(plugins.Plugin):
         if 'ip' not in self.options or ('ip' in self.options and self.options['ip'] is None):
             ip = "192.168.44.1:8080"
         else:
-            ip = self.options['ip]
+            ip = self.options['ip']
 
         gps = requests.get('http://' + ip + '/gps.xhtml')
         gps_filename = filename.replace('.pcap', '.gps.json')
