f563d71477e58632c2a0fbde2c14604f3ce18d2d
hmax42 <maximilian.hoetzl@gmail.com>
2019-12-01 14:27:13 +0100

    Update gps.py

--- pwnagotchi/plugins/default/gps.py
+++ pwnagotchi/plugins/default/gps.py
@@ -55,9 +55,9 @@ class GPS(plugins.Plugin):
             lon_pos = (122, 84)
             alt_pos = (127, 94)
         elif ui.is_waveshare_v1():
-            lat_pos = (127, 75)
-            lon_pos = (122, 84)
-            alt_pos = (127, 94)
+            lat_pos = (130, 70)
+            lon_pos = (125, 80)
+            alt_pos = (130, 90)
         elif ui.is_inky():
             # guessed values, add tested ones if you can
             lat_pos = (112, 30)
