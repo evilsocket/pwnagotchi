b4daf19401783d5bdd9f54fe47833fa871aee9b8
Evg33 <evg33@evg33.ru>
2019-12-06 20:11:37 +0300

    layout redesign

--- pwnagotchi/ui/hw/waveshare144lcd.py
+++ pwnagotchi/ui/hw/waveshare144lcd.py
@@ -16,11 +16,11 @@ class Waveshare144lcd(DisplayImpl):
         self._layout['face'] = (0, 43)
         self._layout['name'] = (0, 14)
         self._layout['channel'] = (0, 0)
-        self._layout['aps'] = (30, 0)
+        self._layout['aps'] = (0, 71)
         self._layout['uptime'] = (0, 25)
         self._layout['line1'] = [0, 12, 127, 12]
         self._layout['line2'] = [0, 116, 127, 116]
-        self._layout['friend_face'] = (14, 78)
+        self._layout['friend_face'] = (12, 88)
         self._layout['friend_name'] = (1, 103)
         self._layout['shakes'] = (26, 117)
         self._layout['mode'] = (0, 117)
