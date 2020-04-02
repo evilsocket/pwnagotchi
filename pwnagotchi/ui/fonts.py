3b9aacdd161615a6faaf000690277a3cfae18249
dadav <33197631+dadav@users.noreply.github.com>
2020-04-02 19:24:52 +0200

    faces use dejavu

--- pwnagotchi/ui/fonts.py
+++ pwnagotchi/ui/fonts.py
@@ -1,34 +1,43 @@
 from PIL import ImageFont
 
 FONT_NAME = None
+FONT_NAME_FACES = 'DejaVuSansMono'
+
+SIZE_OFFSET = 0
 Bold = None
 BoldSmall = None
 BoldBig = None
 Medium = None
 Small = None
 Huge = None
+FaceHuge = None
+FaceBold = None
 
 
 def init(config):
-    global FONT_NAME
-    FONT_NAME = config['ui']['font']
+    global FONT_NAME, SIZE_OFFSET
+    FONT_NAME = config['ui']['font']['name']
+    SIZE_OFFSET = config['ui']['font']['size_offset']
     setup(10, 8, 10, 25, 25, 9)
 
 
 def setup(bold, bold_small, medium, huge, bold_big, small):
-    global Bold, BoldSmall, Medium, Huge, BoldBig, Small, FONT_NAME
+    global Bold, BoldSmall, Medium, Huge, BoldBig, Small, FaceHuge, FaceBold,
+           FONT_NAME, SIZE_OFFSET
 
-    Small = ImageFont.truetype("%s.ttf" % FONT_NAME, small)
-    Medium = ImageFont.truetype("%s.ttf" % FONT_NAME, medium)
+    Small = ImageFont.truetype("%s.ttf" % FONT_NAME, small + SIZE_OFFSET)
+    Medium = ImageFont.truetype("%s.ttf" % FONT_NAME, medium, + SIZE_OFFSET)
+    FaceHuge = ImageFont.truetype("%s-Bold.ttf" % FONT_NAME_FACES, huge)
+    FaceBold = ImageFont.truetype("%s-Bold.ttf" % FONT_NAME_FACES, bold)
 
     try:
-        BoldSmall = ImageFont.truetype("%s-Bold.ttf" % FONT_NAME, bold_small)
-        Bold = ImageFont.truetype("%s-Bold.ttf" % FONT_NAME, bold)
-        BoldBig = ImageFont.truetype("%s-Bold.ttf" % FONT_NAME, bold_big)
-        Huge = ImageFont.truetype("%s-Bold.ttf" % FONT_NAME, huge)
+        BoldSmall = ImageFont.truetype("%s-Bold.ttf" % FONT_NAME, bold_small + SIZE_OFFSET)
+        Bold = ImageFont.truetype("%s-Bold.ttf" % FONT_NAME, bold + SIZE_OFFSET)
+        BoldBig = ImageFont.truetype("%s-Bold.ttf" % FONT_NAME, bold_big + SIZE_OFFSET)
+        Huge = ImageFont.truetype("%s-Bold.ttf" % FONT_NAME, huge + SIZE_OFFSET)
     except OSError:
-        BoldSmall = ImageFont.truetype("%s.ttf" % FONT_NAME, bold_small)
-        Bold = ImageFont.truetype("%s.ttf" % FONT_NAME, bold)
-        BoldBig = ImageFont.truetype("%s.ttf" % FONT_NAME, bold_big)
-        Huge = ImageFont.truetype("%s.ttf" % FONT_NAME, huge)
+        BoldSmall = ImageFont.truetype("%s.ttf" % FONT_NAME, bold_small + SIZE_OFFSET)
+        Bold = ImageFont.truetype("%s.ttf" % FONT_NAME, bold + SIZE_OFFSET)
+        BoldBig = ImageFont.truetype("%s.ttf" % FONT_NAME, bold_big + SIZE_OFFSET)
+        Huge = ImageFont.truetype("%s.ttf" % FONT_NAME, huge + SIZE_OFFSET)
 
