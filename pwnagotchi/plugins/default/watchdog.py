6038f555fa7fb52b5eb5ddf1138aaa804f3c48b0
dadav <33197631+dadav@users.noreply.github.com>
2020-04-18 00:02:25 +0200

    fix

--- pwnagotchi/plugins/default/watchdog.py
+++ pwnagotchi/plugins/default/watchdog.py
@@ -3,8 +3,6 @@ import logging
 import re
 import subprocess
 from io import TextIOWrapper
-from time import sleep
-from threading import Lock
 from pwnagotchi import plugins
 
 
@@ -16,7 +14,6 @@ class Watchdog(plugins.Plugin):
 
     def __init__(self):
         self.options = dict()
-        self.lock = Lock()
         self.pattern = re.compile(r'brcmf_cfg80211_nexmon_set_channel.*?Set Channel failed')
 
     def on_loaded(self):
@@ -26,18 +23,14 @@ class Watchdog(plugins.Plugin):
         logging.info("Watchdog plugin loaded.")
 
     def on_epoch(self, agent, epoch, epoch_data):
-        if self.lock.locked():
-            return
-        with self.lock:
-            # get last 10 lines
-            last_lines = ''.join(list(TextIOWrapper(subprocess.Popen(['journalctl','-n10','-k'],
-                                                  stdout=subprocess.PIPE).stdout))[-10:])
-            if len(self.pattern.findall(last_lines)) >= 3:
-                display = agent.view()
-                display.set('status', 'Blind-Bug detected. Restarting bettercap.')
-                display.update(force=True)
-                logging.info('[WATCHDOG] Blind-Bug detected. Restarting.')
-                mode_file = '/root/.pwnagotchi-manual' if agent.mode == 'manual' else '/root/.pwnagotchi-auto'
-                os.system(f"touch {mode_file}")
-                os.system('systemctl restart bettercap')
-                sleep(10)
+        # get last 10 lines
+        last_lines = ''.join(list(TextIOWrapper(subprocess.Popen(['journalctl','-n10','-k'],
+                                                stdout=subprocess.PIPE).stdout))[-10:])
+        if len(self.pattern.findall(last_lines)) >= 3:
+            display = agent.view()
+            display.set('status', 'Blind-Bug detected. Restarting bettercap.')
+            display.update(force=True)
+            logging.info('[WATCHDOG] Blind-Bug detected. Restarting.')
+            mode = 'MANU' if agent.mode == 'manual' else 'AUTO'
+            import pwnagotchi
+            pwnagotchi.restart(mode=mode)
