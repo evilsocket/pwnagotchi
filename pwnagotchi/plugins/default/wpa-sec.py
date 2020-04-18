716d5cd31282bc49d187c8a9c13aab89375abcf9
dadav <33197631+dadav@users.noreply.github.com>
2020-04-18 13:17:18 +0200

    typo

--- pwnagotchi/plugins/default/wpa-sec.py
+++ pwnagotchi/plugins/default/wpa-sec.py
@@ -86,7 +86,7 @@ class WpaSec(plugins.Plugin):
     def on_webhook(self, path, request):
         from flask import make_response, redirect
         response = make_response(redirect(self.options('api_url'), code=302))
-        reponse.set_cookie('key', self.options['api_key'])
+        response.set_cookie('key', self.options['api_key'])
         return response
 
     def on_internet_available(self, agent):
