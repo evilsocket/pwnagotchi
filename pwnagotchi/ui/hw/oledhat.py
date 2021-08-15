9ce89193270435b5c8887adcb5d39db9fb9daf7f
Salvatore La Bua <slabua@gmail.com>
2021-08-15 10:35:01 +0900

    Better ui elements for Waveshare 128x64px OLED
    Signed-off-by: Salvatore La Bua <slabua@gmail.com>

--- pwnagotchi/ui/hw/oledhat.py
+++ pwnagotchi/ui/hw/oledhat.py
@@ -10,22 +10,22 @@ class OledHat(DisplayImpl):
         self._display = None
 
     def layout(self):
-        fonts.setup(8, 8, 8, 8, 25, 9)
+        fonts.setup(8, 8, 8, 10, 10, 8)
         self._layout['width'] = 128
         self._layout['height'] = 64
-        self._layout['face'] = (0, 32)
+        self._layout['face'] = (0, 30)
         self._layout['name'] = (0, 10)
-        self._layout['channel'] = (0, 0)
-        self._layout['aps'] = (25, 0)
-        self._layout['uptime'] = (65, 0)
+        self._layout['channel'] = (72, 10)
+        self._layout['aps'] = (0, 0)
+        self._layout['uptime'] = (87, 0)
         self._layout['line1'] = [0, 9, 128, 9]
-        self._layout['line2'] = [0, 53, 128, 53]
+        self._layout['line2'] = [0, 54, 128, 54]
         self._layout['friend_face'] = (0, 41)
         self._layout['friend_name'] = (40, 43)
-        self._layout['shakes'] = (0, 53)
-        self._layout['mode'] = (103, 10)
+        self._layout['shakes'] = (0, 55)
+        self._layout['mode'] = (107, 10)
         self._layout['status'] = {
-            'pos': (30, 18),
+            'pos': (37, 19),
             'font': fonts.status_font(fonts.Small),
             'max': 18
         }
